9cea2c75d434911849b7a38bc5e25e532ed8790c
Zach <zrauch@purdue.edu>
2021-01-30 16:44:39 -0500

    test2

--- conversions.py
+++ conversions.py
@@ -1,21 +1,7 @@
 # Import libraries
-import numpy as np # NumPy: contains basic numerical routines
-import scipy # SciPy: contains additional numerical routines to numpy
-import scipy.sparse as scysparse
-import scipy.sparse.linalg
-import re
-import itertools # for permutations
+import numpy as np
+import scipy
 import os
-import matplotlib.pyplot as plt
-import math
-from math import pi as PI
-from scipy.optimize import fsolve
-from matplotlib.font_manager import FontProperties
-from sympy import symbols, solve
-import os
-import CoolProp
-import CoolProp.CoolProp as CP
-from CoolProp.CoolProp import PropsSI as prop
 
 ## define standard constant variables
 p_ref = 101325
--- properties.py
+++ properties.py
@@ -1,39 +1,31 @@
 # Import libraries
+import sys,os
+sys.path.append('~/phd-work/graduate-coursework/pyTherm/')
+import conversions
+
 import numpy as np # NumPy: contains basic numerical routines
 import scipy # SciPy: contains additional numerical routines to numpy
-import scipy.sparse as scysparse
-import scipy.sparse.linalg
-import re
-import itertools # for permutations
-import os
 import matplotlib.pyplot as plt
-import math
-from math import pi as PI
-from scipy.optimize import fsolve
-from matplotlib.font_manager import FontProperties
-from sympy import symbols, solve
-import os
 import CoolProp
-import CoolProp.CoolProp as CP
 from CoolProp.CoolProp import PropsSI as prop
 
 ### Fluid Property Calculations Based on State Conditions
 def waterProperties(propertyNames, propertyValues):
 	if propertyNames[0]=='P' and propertyNames[1]=='Q':
-		rho = prop('D',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # kg/m3
+		rho = prop('D',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # kg/m3
 		v = 1/rho # m3/kg
-		temp = prop('T',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # K
-		intEnergy = prop('U',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # J/kg
-		enthalpy = prop('H',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # J/kg
-		entropy = prop('S',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # J/kg-K
+		temp = prop('T',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # K
+		intEnergy = prop('U',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # J/kg
+		enthalpy = prop('H',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # J/kg
+		entropy = prop('S',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # J/kg-K
 
 	elif propertyNames[0]=='P' and propertyNames[1]=='D':
 		rho = propertyValues[1]
 		v = 1/rho # m3/kg
-		temp = prop('T',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # K
-		intEnergy = prop('U',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg
-		enthalpy = prop('H',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg
-		entropy = prop('S',propertyNames[0],BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg-K
+		temp = prop('T',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # K
+		intEnergy = prop('U',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg
+		enthalpy = prop('H',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg
+		entropy = prop('S',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg-K
 
 	print("Water Vapor Properties (SI UNITS)")
 	print("===========================================")
