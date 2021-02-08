6b0af0697b9ab8428a0fcb1b0172a7f881f218c2
Zach <zrauch@purdue.edu>
2021-02-07 19:59:33 -0500

    beginning of ideal gas properties function incldued

--- conversions.py
+++ conversions.py
@@ -3,12 +3,6 @@ import numpy as np
 import scipy
 import os
 
-## define standard constant variables
-p_ref = 101325 # Pa
-T_ref = 298.15 # K
-R_air = 287 # J/kg-K
-
-
 def KtoC(temp):
 	temp-=273.15
 	return temp
@@ -20,3 +14,7 @@ def CtoK(temp):
 def BarToPa(pres):
 	pres*=100000
 	return pres
+
+def cm3Tom3(vol):
+	vol*=1e-6
+	return vol
--- properties.py
+++ properties.py
@@ -9,8 +9,35 @@ import matplotlib.pyplot as plt
 import CoolProp
 from CoolProp.CoolProp import PropsSI as prop
 
+
+## define standard constant variables
+p_ref = 101325 # Pa
+T_ref = 298.15 # K
+R_air = 287 # J/kg-K
+
+
+def airProperties(propertyNames, propertyValues, printOutput):
+	if propertyNames[0]=='P' and propertyNames[1]=='T':
+		u = prop('U',propertyNames[0],propertyValues[0],propertyNames[1],propertyValues[1],'Air')
+		h = prop('H',propertyNames[0],propertyValues[0],propertyNames[1],propertyValues[1],'Air')
+		s = prop('S',propertyNames[0],propertyValues[0],propertyNames[1],propertyValues[1],'Air')
+		rho = prop('D',propertyNames[0],propertyValues[0],propertyNames[1],propertyValues[1],'Air')
+
+	if printOutput:
+		print("Air Properties (SI UNITS)")
+		print("===========================================")
+		print("Interal Energy [J/kg] =",u)
+		print("Enthalpy [J/kg] =",h)
+		print("Entropy [J/kg-K] =",s)
+		print("Density [kg/m3] =",rho)
+		print("\n")
+
+	return [u,h,s]
+
+
+
 ### Fluid Property Calculations Based on State Conditions
-def waterProperties(propertyNames, propertyValues):
+def waterProperties(propertyNames, propertyValues, printOutput):
 	if propertyNames[0]=='P' and propertyNames[1]=='Q':
 		rho = prop('D',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],propertyValues[1],'Water') # kg/m3
 		v = 1/rho # m3/kg
@@ -27,13 +54,14 @@ def waterProperties(propertyNames, propertyValues):
 		enthalpy = prop('H',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg
 		entropy = prop('S',propertyNames[0],conversions.BarToPa(propertyValues[0]),propertyNames[1],rho,'Water') # J/kg-K
 
-	print("Water Vapor Properties (SI UNITS)")
-	print("===========================================")
-	print("Density [rho] =\t\t",rho,"kg/m3")
-	print("Specific Volume [v] =\t",v,"m3/kg")
-	print("Temperature [T] =\t",temp,"K")
-	print("Internal Energy [u] =\t",intEnergy,"J/kg")
-	print("Enthalpy [h] =\t\t",enthalpy,"J/kg")
-	print("Entropy [s] =\t\t",entropy,"J/kg-K\n")
+	if printOutput:
+		print("Water Vapor Properties (SI UNITS)")
+		print("===========================================")
+		print("Density [rho] =\t\t",rho,"kg/m3")
+		print("Specific Volume [v] =\t",v,"m3/kg")
+		print("Temperature [T] =\t",temp,"K")
+		print("Internal Energy [u] =\t",intEnergy,"J/kg")
+		print("Enthalpy [h] =\t\t",enthalpy,"J/kg")
+		print("Entropy [s] =\t\t",entropy,"J/kg-K\n")
 
 	return [rho,v,temp,intEnergy,enthalpy,entropy]
